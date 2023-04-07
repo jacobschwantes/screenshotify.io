7a73d735bac59a364dd80e65063af4d86e7b0cc7
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2023-04-06 22:59:45 -0500

    feat: switch to mdx for terms/privacy

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,20 +1,11 @@
-import Script from "next/script";
-const Privacy = () => (
-  <div className="w-full h-full bg-black">
-    <div
-      className="bg-black max-w-6xl mx-auto pt-16"
-      //@ts-ignore
-      name="termly-embed"
-      data-id="1fa34cc3-1c4f-4c9a-aac1-5beb22eaef48"
-      data-type="iframe"
-    ></div>
-    <Script id="privacy" type="text/javascript">{`(function(d, s, id) {
-  var js, tjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://app.termly.io/embed-policy.min.js";
-  tjs.parentNode.insertBefore(js, tjs);
-}(document, 'script', 'termly-jssdk'));`}</Script>
-  </div>
-);
-export default Privacy;
+import { MDXProvider } from "@mdx-js/react";
+import Privacy from "../mdx/privacy.mdx";
+export default function Page() {
+  return (
+    <MDXProvider>
+      <div className="dark:prose-invert prose min-h-screen max-w-6xl mx-auto py-28 px-4">
+        <Privacy />
+      </div>
+    </MDXProvider>
+  );
+}
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,20 +1,11 @@
-import Script from "next/script";
-const Terms = () => (
-  <div className="w-full h-full bg-black">
-    <div
-      className="bg-black max-w-6xl mx-auto pt-16"
-      //@ts-ignore
-      name="termly-embed"
-      data-id="d84722d7-35f6-41f0-a416-bdb04899789c"
-      data-type="iframe"
-    ></div>
-    <Script id="terms" type="text/javascript">{`(function(d, s, id) {
-  var js, tjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://app.termly.io/embed-policy.min.js";
-  tjs.parentNode.insertBefore(js, tjs);
-}(document, 'script', 'termly-jssdk'));`}</Script>
-  </div>
-);
-export default Terms;
+import { MDXProvider } from '@mdx-js/react'
+import Terms from '../mdx/terms.mdx'
+export default function Page() {
+  return (
+    <MDXProvider >
+     <div className='dark:prose-invert prose min-h-screen max-w-6xl mx-auto py-28 px-4 '>
+      <Terms/>
+     </div>
+    </MDXProvider>
+  )
+}
